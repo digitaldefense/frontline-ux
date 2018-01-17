5f47dc3903b9e5581877b083b37589f0382879dc
Ben Thompson <smoov.b@gmail.com>
2018-01-17 09:16:53 -0600

    extend HasElementRef with optional properties

--- src/lib/core/common-behaviors/color.ts
+++ src/lib/core/common-behaviors/color.ts
@@ -7,7 +7,9 @@
  */
 
 import {Constructor} from './constructor';
-import {ElementRef} from '@angular/core';
+import {ElementRef, Renderer2} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { FlxThemeService, FlxTheme } from '../theme/index';
 
 /** @docs-private */
 export interface CanColor {
@@ -18,16 +20,23 @@ export interface CanColor {
 /** @docs-private */
 export interface HasElementRef {
   _elementRef: ElementRef;
+  _renderer?: Renderer2;
+  _themeSvc?: FlxThemeService;
+  _params?: any;
 }
 
 /** Possible color palette values.  */
-export type ThemePalette = 'primary' | 'accent' | 'warn' | undefined;
+// export type ThemePalette = 'primary' | 'accent' | 'warn' | 'primary-x' | 'accent-x' | 'warn-x'
+//   | 'danger' | 'danger-x' | undefined;
+export type ThemePalette = string | undefined;
 
 /** Mixin to augment a directive with a `color` property. */
 export function mixinColor<T extends Constructor<HasElementRef>>(base: T,
     defaultColor?: ThemePalette): Constructor<CanColor> & T {
   return class extends base {
     private _color: ThemePalette;
+
+    private _subscription: Subscription;
 
     get color(): ThemePalette { return this._color; }
     set color(value: ThemePalette) {
@@ -50,6 +59,33 @@ export function mixinColor<T extends Constructor<HasElementRef>>(base: T,
 
       // Set the default color that can be specified from the mixin.
       this.color = defaultColor;
+
+      if (this._themeSvc !== undefined) {
+        this._subscription = this._themeSvc.theme.subscribe((theme: FlxTheme) => {
+          this._applyThemeColors(theme);
+        });
+      }
+    }
+
+    private _applyThemeColors(theme) {
+      if (this._renderer === undefined) { return; }
+
+      const elem = this._elementRef.nativeElement;
+      const color = (this._color === undefined) ? 'primary' : this._color;
+      if (this._isTextNode(elem)) {
+        this._renderer.setStyle(elem, 'color', theme[color]);
+      } else {
+        const reverse = color + 'Contrast';
+        this._renderer.setStyle(elem, 'background-color', theme[color]);
+        this._renderer.setStyle(elem, 'color', theme[reverse]);
+      }
+    }
+
+    private _isTextNode(element: HTMLElement): Boolean {
+      const textNodes = [
+        'P', 'H1', 'H2', 'H3', 'H4', 'H5', 'SPAN', 'I', 'STRONG', 'EM', 'B', 'A'
+      ];
+      return textNodes.indexOf(element.tagName) !== -1;
     }
   };
 }
