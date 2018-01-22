103b6d9183ccd711a2499170a78d3f4df8b91aae
Ben Thompson <smoov.b@gmail.com>
2018-01-22 12:37:32 -0600

    automatically apply color to background or text based on element type

--- src/lib/color/color.ts
+++ src/lib/color/color.ts
@@ -14,13 +14,15 @@ import {
   Renderer2 } from '@angular/core';
 import { FlxThemeService, FlxTheme } from '@angular/material/core';
 
-// type ThemePalette = 'primary' | 'accent' | 'warning' | 'danger' | 'success';
+const textNodes = ['H1', 'H2', 'H3', 'H4', 'H5', 'P', 'SPAN', 'I', 'STRONG', 'EM', 'B'];
 
 @Directive({
   selector: '[fcolor]',
 })
 export class FlxColor {
   private _theme: FlxTheme;
+  private _color: string;
+  private _contrast: string;
 
   constructor(
     private _element: ElementRef,
@@ -32,11 +34,35 @@ export class FlxColor {
     }
 
   private _setElemColor(color: string) {
-    const elem = this._element.nativeElement;
     if (color.charAt(0) === '#') {
       // Support custom Hex colors
+      this._color = color;
     } else {
-      this._renderer.setStyle(elem, 'background-color', this._theme[color]);
+      let contrast = color + 'Contrast';
+      this._color = this._theme[color];
+      this._contrast = this._theme[contrast];
     }
+    this._renderElemColor();
+  }
+
+  /** Applies the color to either the background or text based on element type */
+  private _renderElemColor() {
+    const elem = this._element.nativeElement;
+    if (this._isTextNode(this._element.nativeElement)) {
+      this._renderer.setStyle(elem, 'color', this._color);
+    } else {
+      this._renderer.setStyle(elem, 'background-color', this._color);
+      if (this._contrast != null) {
+        this._renderer.setStyle(elem, 'color', this._contrast);
+      }
+    }
+  }
+
+  private _isTextNode(element: HTMLElement): Boolean {
+    if (element.tagName === 'BUTTON' || element.tagName === 'A') {
+      const classList = element.classList;
+      return Boolean(classList.contains('mat-button') || classList.contains('mat-icon-button'));
+    }
+    return textNodes.indexOf(element.tagName) !== -1;
   }
 }
